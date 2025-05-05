3963c157072348d607a0f8928b8f9b81164ddda8
Shashank <shashank78680@gmail.com>
2025-05-06 01:12:57 +0530

    routers defined in main file

--- server/main.py
+++ server/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
 
+from auth.routes import auth_router
+from pdf.routes import pdfRouter
+from chat.routes import chatRouter
+
 app = FastAPI()
+
+app.include_router(auth_router)
+app.include_router(pdfRouter)
+app.include_router(chatRouter)
 
 @app.get("/")
 async def index():
