321908012abaa31177149edffff6ad7814a68cb7
Shashank <shashank78680@gmail.com>
2025-05-29 23:37:23 +0530

    added cors on backend

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.auth.routes import auth_router
 from src.docs_ingestion.routes import documents_router
@@ -18,6 +19,14 @@ app = FastAPI(
     description="API for PDF assistant",
     version=version,
     lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=["Authentication"])
