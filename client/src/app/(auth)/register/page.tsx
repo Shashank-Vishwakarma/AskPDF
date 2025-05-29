4f9b5ca200f8c809c499d9ea6226cc15250640dd
Shashank <shashank78680@gmail.com>
2025-05-29 23:55:08 +0530

    register page with register api working

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -8,14 +8,47 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { Separator } from "@/components/ui/separator"
 import { ArrowLeft } from "lucide-react"
+import axios from "axios"
+import { useAuthStore, UserProps } from "@/zustand/store"
+import { useRouter } from "next/navigation"
 
 export default function SignupPage() {
     const [isLoading, setIsLoading] = useState(false)
 
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    const setUser = useAuthStore((state) => state.setUser)
+
+    const router = useRouter()
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        
+        e.preventDefault()
+        setIsLoading(true)
+
+        try {
+            const response = await axios.post("http://localhost:8000/api/v1/auth/register", {name, email, password, confirm_password: confirmPassword})
+            if(!response.data) {
+                throw new Error("Something went wrong")
+            }
+
+            const user: UserProps = {
+                name: response.data.name,
+                email: response.data.email,
+                token: response.data.token
+            }
+            setUser(user)
+            localStorage.setItem("user", JSON.stringify(user))
+
+            router.push("/dashboard")
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false)
+        }
     }
 
     return (
@@ -34,16 +67,48 @@ export default function SignupPage() {
                     <CardContent>
                         <form onSubmit={handleSubmit} className="space-y-4">
                             <div className="space-y-2">
+                                <Label htmlFor="name">Name</Label>
+                                <Input 
+                                    id="name" 
+                                    type="text" 
+                                    placeholder="John Doe" 
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)} 
+                                    required 
+                                />
+                            </div>
+                            <div className="space-y-2">
                                 <Label htmlFor="email">Email</Label>
-                                <Input id="email" type="email" placeholder="you@example.com" required />
+                                <Input 
+                                    id="email" 
+                                    type="email" 
+                                    placeholder="you@example.com" 
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)} 
+                                    required 
+                                />
                             </div>
                             <div className="space-y-2">
                                 <Label htmlFor="password">Password</Label>
-                                <Input id="password" type="password" placeholder="••••••••" required />
+                                <Input 
+                                    id="password" 
+                                    type="password" 
+                                    placeholder="••••••••" 
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)} 
+                                    required 
+                                />
                             </div>
                             <div className="space-y-2">
                                 <Label htmlFor="confirmPassword">Confirm Password</Label>
-                                <Input id="confirmPassword" type="password" placeholder="••••••••" required />
+                                <Input 
+                                    id="confirmPassword" 
+                                    type="password" 
+                                    placeholder="••••••••" 
+                                    value={confirmPassword}
+                                    onChange={(e) => setConfirmPassword(e.target.value)} 
+                                    required 
+                                />
                             </div>
                             <Button type="submit" className="w-full bg-purple-600 hover:bg-purple-700 cursor-pointer" disabled={isLoading}>
                                 {isLoading ? "Creating account..." : "Sign up"}
