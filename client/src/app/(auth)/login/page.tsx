447fc273310ce60811ffb312f13d52ad41161b9e
Shashank <shashank78680@gmail.com>
2025-05-29 23:38:49 +0530

    login page integrated with api

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -8,14 +8,46 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { Separator } from "@/components/ui/separator"
 import { ArrowLeft } from "lucide-react"
+import axios from "axios"
+import { useAuthStore } from "@/zustand/store"
+import { UserProps } from "@/zustand/store"
+import { useRouter } from "next/navigation"
 
 export default function LoginPage() {
     const [isLoading, setIsLoading] = useState(false)
 
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const setUser = useAuthStore((state) => state.setUser)
+
+    const router = useRouter()
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        
+        e.preventDefault()
+        setIsLoading(true)
+
+        try {
+            const response = await axios.post("http://localhost:8000/api/v1/auth/login", {email, password})
+            if(!response.data) {
+                throw new Error("Something went wrong")
+            }
+
+            const user: UserProps = {
+                name: response.data.name,
+                email: response.data.email,
+                token: response.data.token
+            }
+            setUser(user)
+            localStorage.setItem("user", JSON.stringify(user))
+
+            router.push("/dashboard")
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false)
+        }
     }
 
     return (
@@ -35,11 +67,25 @@ export default function LoginPage() {
                         <form onSubmit={handleSubmit} className="space-y-4">
                             <div className="space-y-2">
                                 <Label htmlFor="email">Email</Label>
-                                <Input id="email" type="email" placeholder="you@example.com" required />
+                                <Input 
+                                    id="email" 
+                                    type="email" 
+                                    placeholder="you@example.com" 
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required 
+                                />
                             </div>
                             <div className="space-y-2">
                                 <Label htmlFor="password">Password</Label>
-                                <Input id="password" type="password" placeholder="••••••••" required />
+                                <Input 
+                                    id="password" 
+                                    type="password" 
+                                    placeholder="••••••••" 
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required 
+                                />
                             </div>
                             <Button type="submit" className="w-full bg-purple-600 hover:bg-purple-700 cursor-pointer" disabled={isLoading}>
                                 {isLoading ? "Logging in..." : "Log in"}
--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,45 +1,50 @@
+"use client"
+
 import { User2 } from "lucide-react";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import Link from "next/link";
-
-const tabs = [
-    {
-        label: "Pricing",
-        href: "/pricing",
-    },
-]
+import { useAuthStore } from "@/zustand/store";
+import { Button } from "./ui/button";
 
 export default function Navbar() {
+    const user = useAuthStore((state) => state.user);
+    console.log(user)
+
     return (
         <header className="w-full h-18 sticky flex items-center justify-between border-b border-gray-200 bg-white/60 px-6 py-2">
             <Link href="/">
                 <h3 className="text-2xl font-bold">AskPDF</h3>
             </Link>
 
-            <div>
-                {
-                    tabs.map((tab) => (
-                        <div key={tab.href}>
-                            <Link href={tab.href} className="text-blue-600">
-                                {tab.label}
-                            </Link>
-                        </div>
-                    ))
-                }
-            </div>
-
-            <div>
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <User2 className="rounded-full w-8 h-8 bg-gray-300 p-2 cursor-pointer" />
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuItem>
-                            <p>Logout</p>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </div>
+            {
+                user ? (
+                    <div>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <User2 className="rounded-full w-8 h-8 bg-gray-300 p-2 cursor-pointer" />
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <Link href="/dashboard">
+                                    <DropdownMenuItem>
+                                        <p>Dashboard</p>
+                                    </DropdownMenuItem>
+                                </Link>
+                                <DropdownMenuItem>
+                                    <p>Logout</p>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+                ) : (
+                    <div>
+                        <Link href="/login">
+                            <Button>
+                                Login
+                            </Button>
+                        </Link>
+                    </div>
+                )
+            }
         </header>
     )
 }
