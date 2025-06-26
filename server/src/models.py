8e3510c69cb23319cf5882be699a06b30e2887c7
Shashank <shashank78680@gmail.com>
2025-06-27 00:37:43 +0530

    chats model added

--- server/src/models.py
+++ server/src/models.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, Field, Column, Relationship
+from sqlmodel import SQLModel, Field, Column
 import sqlalchemy.dialects.postgresql as pg
 import uuid
 from datetime import datetime
@@ -45,9 +45,28 @@ class Document(SQLModel, table=True):
     insert_status: bool = False
 
     user_id: Optional[uuid.UUID]  = Field(default=None, foreign_key="users.id")
-    # user: User = Relationship(back_populates="users")
 
     created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now(), nullable=False))
 
     def __repr__(self) -> str:
         return f"<Document {self.pdf_name}>"
+
+class Chat(SQLModel, table=True):
+    __tablename__ = "chats"
+
+    id: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID(as_uuid=True),
+            primary_key=True,
+            unique=True,
+            nullable=False,
+            default=uuid.uuid4,
+        )
+    )
+
+    user_id: Optional[uuid.UUID]  = Field(default=None, foreign_key="users.id")
+    pdf_id: Optional[uuid.UUID] = Field(default=None, foreign_key="documents.id")
+    role: str = Field(sa_column=Column(pg.ENUM("user", "assistant", name="role"), nullable=False))
+    content: str
+
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now(), nullable=False))
